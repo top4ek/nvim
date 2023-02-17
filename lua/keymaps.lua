b5d2de24023ea7794cd75731eced71dec77e241d
Alexander <aaleschenko@freeconferencecall.com>
2023-02-17 10:11:45 +0300

    Bring bufdel back

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,6 +8,21 @@ function Set_keymap(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+Set_keymap("n", "<Leader>q", "",  {
+  callback = function()
+    local buflisted = vim.fn.getbufinfo({buflisted = 1})
+    local cur_winnr, cur_bufnr = vim.fn.winnr(), vim.fn.bufnr()
+    if #buflisted < 2 then cmd 'confirm qall' return end
+    for _, winid in ipairs(vim.fn.getbufinfo(cur_bufnr)[1].windows) do
+      vim.cmd(string.format('%d wincmd w', vim.fn.win_id2win(winid)))
+      vim.cmd(cur_bufnr == buflisted[#buflisted].bufnr and 'bp' or 'bn')
+    end
+    vim.cmd(string.format('%d wincmd w', cur_winnr))
+    local is_terminal = vim.fn.getbufvar(cur_bufnr, '&buftype') == 'terminal'
+    vim.cmd(is_terminal and 'bd! #' or 'silent! confirm bd #')
+  end
+})
+
 Set_keymap("", "<Left>", ':echoe "Use h"<CR>')
 Set_keymap("", "<Right>", ':echoe "Use l"<CR>')
 Set_keymap("", "<Up>", ':echoe "Use k"<CR>')
