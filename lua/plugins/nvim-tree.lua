15d92fe5d6508c0eab03e9caabf702af383e7ed8
Alexander <alex.aleschenko@gmail.com>
2025-04-21 16:57:06 +0300

    Fix tree root sync

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,5 +1,9 @@
 return {
   "nvim-tree/nvim-tree.lua",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
   config = function()
     require("nvim-tree").setup {
       sync_root_with_cwd = true,
