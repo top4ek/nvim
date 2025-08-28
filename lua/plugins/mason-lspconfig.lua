328d17da56a1b5d929f3dba3d14b3806e28f9fa6
Alexander <alex.aleschenko@gmail.com>
2025-08-28 20:22:44 +0300

    Ensure installed with mason

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -6,6 +6,28 @@ return {
   },
   cond = not os.getenv("LIGHT_NVIM"),
   opts = {
+    ensure_installed = {
+      'angularls',
+      'ansiblels',
+      'bashls',
+      'clangd',
+      'cssls',
+      'dockerls',
+      'eslint',
+      'gopls',
+      'html',
+      'jsonls',
+      'lua_ls',
+      'marksman',
+      'pylsp',
+      'ruby_lsp',
+      'rust_analyzer',
+      'sqlls',
+      'stimulus_ls',
+      'tailwindcss',
+      'ts_ls',
+      'yamlls'
+    },
     automatic_enable = {
       'angularls',
       'ansiblels',
