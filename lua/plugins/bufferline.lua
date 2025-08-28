08766570e7a620bab11ab679f30e17fe89804dae
Alexander <alex.aleschenko@gmail.com>
2025-08-28 20:22:23 +0300

    Fix bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -6,7 +6,7 @@ return {
   },
   config = function()
     require("bufferline").setup {
-      highlights = require("catppuccin.groups.integrations.bufferline").get()
+      highlights = require("catppuccin.groups.integrations.bufferline").get_theme()
     }
   end
 }
